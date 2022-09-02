9bc9c116a02c1b6efba1a140eee6a2adf0b5cf57
ForteScarlet <ForteScarlet@163.com>
2022-09-02 23:44:01 +0800

    build: update version to v0.1.7

--- buildSrc/src/main/kotlin/Version.kt
+++ buildSrc/src/main/kotlin/Version.kt
@@ -1,6 +1,6 @@
 object Version {
     const val GROUP = "love.forte.catcode2"
-    const val VERSION = "0.1.6"
+    const val VERSION = "0.1.7"
     const val DESCRIPTION = "Cat Code, the spirit of CQ code continues, a cute universal special code."
 }
 
